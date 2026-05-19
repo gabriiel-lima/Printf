09faaa058c86a68bc41e2a4ca12e82c164eda01c
gabriiel-lima <lima.gabriel@outlook.pt>
2026-05-19 16:48:50 +0100

    19/05

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: garodri2 <garodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 09:59:26 by garodri2          #+#    #+#             */
-/*   Updated: 2026/05/18 18:44:19 by garodri2         ###   ########.fr       */
+/*   Updated: 2026/05/19 16:42:18 by garodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,45 +49,64 @@ void	ft_putnbr(int nbr)
 	ft_putchar((nbr % 10) + '0');
 }
 
-void	ft_putnbr_unsgnd(unsigned int nbr)
+void	ft_putnbr_unsgnd(int nbr)
 {
 	if(nbr > 9)
 		ft_putnbr(nbr / 10);
 	ft_putchar((nbr % 10) + '0');
 }
 
+void	ft_puthex(unsigned int nbr, const char c)
+{
+	if(nbr >= 15)
+		ft_puthex(nbr / 16, c);
+	if(c == 'x')
+		ft_putchar("0123456789abcdef"[nbr % 16]);
+	else
+		ft_putchar("0123456789ABCDEF"[nbr % 16]);
+}
+
+void ft_puthex_ptr(unsigned long nbr)
+{
+	if(nbr > 15)
+		ft_puthex_ptr(nbr / 16);	
+	ft_putchar("0123456789abcdef"[nbr % 16]);
+}
+void	ft_putpointer(void *ptr)
+{
+	unsigned long nbr;
+	
+	nbr = (unsigned long)ptr;  
+	if(!ptr)
+		return ft_putstr("(nil)");
+	ft_putstr("0x");
+	ft_puthex_ptr(nbr);
+}
+
 void	check_arg(const char caracter, va_list argument)
 {
 	
 	if(caracter == 'c')
-	{
 		ft_putchar(va_arg(argument, int));
-	}
 	else if(caracter == 's')
-	{
 		ft_putstr(va_arg(argument, char*));
-	}
-	else if(caracter == 'd')
-	{
+	else if(caracter == 'd' || caracter == 'i')
 		ft_putnbr(va_arg(argument, int));
-	}
 	else if(caracter == 'u')
-	{
 		ft_putnbr_unsgnd(va_arg(argument, unsigned int));
+	else if(caracter == 'x')
+		ft_puthex(va_arg(argument, unsigned int), caracter);
+	else if(caracter == 'X')
+		ft_puthex(va_arg(argument, unsigned int), caracter);
+	else if(caracter == 'p')
+		ft_putpointer(va_arg(argument, void *));
+	else if(caracter == '%')
+	{
+		ft_putchar('%');
 	}
-	// switch (c){
-	// 	case 'c':
-	// 		char x = va_arg(argument, int);
-	// 		break;
-	// 	case 's':
-		
-	// 		break;
-	// 	case 'd':
-	// 		break;
-	// }
 }
 
-void	ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
 	int	i;
 
@@ -112,8 +131,8 @@ void	ft_printf(const char *format, ...)
 		ft_putchar(format[i]);
 		i ++;
 	}
-	
 	va_end(arg);
+	return i;
 }
 
 int main(void)
@@ -121,6 +140,10 @@ int main(void)
 	// char *c = "bao?";
 	// ft_putstr("VAI CORINTHIANS");
 	// printf("\nTudo %s Aoba\n", c);
-	ft_printf("%u bem?", -1);
+	// printf("%d", printf("%s", c));
+	int t = ft_printf("ola");
+	ft_printf("%d", t);
+	// printf("%p", c);
+	
 	return (0);
 }
